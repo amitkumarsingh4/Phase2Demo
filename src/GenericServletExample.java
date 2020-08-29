b4d1f1dfdb97174b1fb5053883b031964c2ec0a1
amitkumarsingh4 <amitsinghkumar1@ip-172-31-69-172.ec2.internal>
2020-08-29 17:59:01 +0000

    httpServlet

---
+++ src/GenericServletExample.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class GenericServletExample extends GenericServlet{
+	
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException{
+		
+		//media type
+		response.setContentType("text/html");
+		
+		//generate response	
+		PrintWriter outRes = response.getWriter();
+		
+		outRes.print("<html>");
+		outRes.print("<body>");
+		outRes.print("<h1> Hello, Generic Servlet Example <h1>");
+		outRes.print("<p> Not a protocol restricted request <p>");
+		outRes.print("</body>");
+		outRes.print("</html>");
+		
+	}
+
+}
